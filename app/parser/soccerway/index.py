f6590b770178f7aeefdcb62b8ca5f03e37c0f3a8
Stepan Fedosov <zollidan@yandex.ru>
2024-11-07 13:41:18 +0300

    create: file db create

--- app/parser/soccerway/index.py
+++ app/parser/soccerway/index.py
@@ -1,16 +1,34 @@
+import json
+from fastapi import Depends
 from art import tprint
 import pandas as pd
 import os
+from sqlalchemy.orm import Session
+import requests
+
+from app import models
+from app.database import get_db
+from app.repository import create_file_record
 
 def soccerway(time_date: str):
     
-    file_name = 'soccerway.xlsx'
+    file_name = f'soccerway-{time_date}.xlsx'
     
     data = [[1,2,3], [4,5,6], [time_date]]
     
-    # df = pd.DataFrame(data=data)
+    df = pd.DataFrame(data=data)
     
-    # df.to_excel(file_name, index=False)
+    create_file_record_href = f'https://yandex.bucker.com/myfile/{file_name}'
+
+    create_file_body = json.dumps({
+        "url": create_file_record_href
+    })
+    
+    create_file_record = requests.post(f"http://localhost:8000/files/add", data=create_file_body)
+    
+    df.to_excel(file_name, index=False)
+
+    
 
     # os.remove(file_name)
 
