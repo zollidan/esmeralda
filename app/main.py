20ee4e6841dc9a6d9d2d190a2d47cfa7c2de7cf9
Stepan <zollidan@yandex.ru>
2024-11-11 23:04:31 +0300

    refactor: bot start disable

--- app/main.py
+++ app/main.py
@@ -16,19 +16,19 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 async def lifespan(app: FastAPI):
     print("-"* 15, "LeFort ESMERALDA 2024", "-"* 15)
     tprint('ESMERALDA')
-    dp.include_router(tg_user_router)
+    # dp.include_router(tg_user_router)
     app.include_router(router)
-    await start_bot()
-    webhook_url = settings.get_webhook_url()
-    await bot.set_webhook(url=webhook_url,
-                          allowed_updates=dp.resolve_used_update_types(),
-                          drop_pending_updates=True)
-    logging.info(f"Webhook set to {webhook_url}")
+    # await start_bot()
+    # webhook_url = settings.get_webhook_url()
+    # await bot.set_webhook(url=webhook_url,
+    #                       allowed_updates=dp.resolve_used_update_types(),
+    #                       drop_pending_updates=True)
+    # logging.info(f"Webhook set to {webhook_url}")
     yield
-    logging.info("Shutting down bot...")
-    await bot.delete_webhook()
-    await stop_bot()
-    logging.info("Webhook deleted")
+    logging.info("Shutting down appication...")
+    # await bot.delete_webhook()
+    # await stop_bot()
+    # logging.info("Webhook deleted")
     
 
 
@@ -36,12 +36,12 @@ app = FastAPI(lifespan=lifespan)
 
 
 
-@app.post("/webhook")
-async def webhook(request: Request) -> None:
-    logging.info("Received webhook request")
-    update = Update.model_validate(await request.json(), context={"bot": bot})
-    await dp.feed_update(bot, update)
-    logging.info("Update processed")
+# @app.post("/webhook")
+# async def webhook(request: Request) -> None:
+#     logging.info("Received webhook request")
+#     update = Update.model_validate(await request.json(), context={"bot": bot})
+#     await dp.feed_update(bot, update)
+#     logging.info("Update processed")
 
 # if __name__ == "__main__":
 #     import uvicorn
