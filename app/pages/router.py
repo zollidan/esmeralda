e51359c57552e99a56643dfab9b08da691cdf1d6
Stepan Fedosov <zollidan@yandex.ru>
2024-11-19 14:12:25 +0300

    create: css styles, available status check

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,15 +1,23 @@
 from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 from starlette.responses import FileResponse
+import requests
 
-from app.api.router import get_files 
+from app.api.router import get_files, run_soccerway_test_connection 
 
 pages_router = APIRouter(tags=['Фронтенд'])
 templates = Jinja2Templates(directory='app/templates')
 
 @pages_router.get('/')
 async def get_home_page(request: Request):
-    return templates.TemplateResponse(name='home.html', context={'request': request})
+    
+    response_code = run_soccerway_test_connection()
+    
+    if response_code != 200:
+        connect_message = {"status": response_code, "message": "services unavalible"}
+    else:
+        connect_message = {"status": response_code, "message": "Soccerway services are available"}
+    return templates.TemplateResponse(name='home.html', context={'request': request, "status_validation": connect_message})
 
 @pages_router.get('/parsers')
 async def get_parsers_page(request: Request):
