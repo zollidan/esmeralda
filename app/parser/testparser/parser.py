103e72cd9042b2d4af6c3122a776b2ed9c29b26b
Stepan <zollidan@yandex.ru>
2024-12-19 03:27:19 +0300

    fix: celery import

--- app/parser/testparser/parser.py
+++ app/parser/testparser/parser.py
@@ -1,11 +1,8 @@
 from app.celery_app import celery_app
 from app.parser.testparser.parser_funcs import parser_loop
 
-from celery import Celery
-
-
 # Функция для выполнения через Celery
 @celery_app.task
-def parse_data_test(self):
+def parse_data_test():
     print("[Test Parser start working]")
     parser_loop()  # Здесь вызывается ваша функция парсера
