b156246c7a3c7662017c27290cbe6298a31e1ce5
Stepan Fedosov <zollidan@yandex.ru>
2025-04-28 18:48:49 +0300

    feat: initialize frontend with React, TypeScript, and Vite setup
    - Added basic structure for a React application using Vite.
    - Included Tailwind CSS for styling.
    - Created initial components and pages.
    - Set up ESLint configuration for TypeScript and React.
    - Configured TypeScript settings for app and node environments.
    - Added Docker Compose configuration for backend services.
    - Updated .gitignore to exclude frontend build files and logs.

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <nav className="">
+          <ul>
+            <li>Парсеры</li>
+            <li>Задачи</li>
+            <li>Файлы</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
