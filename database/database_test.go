b3aed5379102bec97744a37d367683169b035474
Stepan Fedosov <zollidan@yandex.ru>
2025-10-26 14:52:05 +0300

    feat: add database migration for Parser model and implement initialization test

---
+++ database/database_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/zollidan/esmeralda/config"
+	"github.com/zollidan/esmeralda/models"
+)
+
+func TestInitDatabase(t *testing.T) {
+	cfg := config.Config{
+		DBURL: ":memory:",
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fatalf("InitDatabase panicked: %v", r)
+		}
+	}()
+
+	database := InitDatabase(cfg)
+	if database == nil {
+		t.Fatal("database is nil after initialization")
+	}
+
+	for _, model := range []interface{}{&models.Files{}, &models.Task{}, &models.Parser{}} {
+		if !database.Migrator().HasTable(model) {
+			t.Errorf("expected table for %T to exist", model)
+		}
+	}
+}
