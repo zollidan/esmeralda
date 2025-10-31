203640e4e334e0f4572f85d436babc46dfaca4c0
Stepan Fedosov <zollidan@yandex.ru>
2025-10-31 03:21:46 +0300

    feat: Refactor TaskManager interface and enhance task management with result processing

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -10,11 +11,22 @@ import (
 	"github.com/zollidan/esmeralda/utils"
 )
 
+type TaskManager interface {
+	StartTask(taskID uint) error
+	StartTaskWithScheduler(taskID uint, cronExpr string)
+	GetResult() error
+	CancelTask(taskID uint) error
+	Shutdown()
+}
+
 type Manager struct {
-	QueueMQ   *amqp091.Queue
-	Scheduler *scheduler.Scheduler
-	mq        *mq.MQ
-	ch        *amqp091.Channel
+	TaskQueue   *amqp091.Queue
+	ResultQueue *amqp091.Queue
+	Scheduler   *scheduler.Scheduler
+	mq          *mq.MQ
+	ch          *amqp091.Channel
+	ctx         context.Context
+	cancel      context.CancelFunc
 }
 
 func NewManager() *Manager {
@@ -24,7 +36,7 @@ func NewManager() *Manager {
 
 	ch := rabbitMQ.GetChannel()
 
-	q, err := ch.QueueDeclare(
+	taskQueue, err := ch.QueueDeclare(
 		"test_queue", // name
 		false,        // durable
 		false,        // delete when unused
@@ -32,21 +44,35 @@ func NewManager() *Manager {
 		false,        // no-wait
 		nil,          // arguments
 	)
-
 	utils.FailOnError(err, "Failed to declare a queue")
 
+	resultQueue, err := ch.QueueDeclare(
+		"test_result_queue", // name
+		false,               // durable
+		false,               // delete when unused
+		false,               // exclusive
+		false,               // no-wait
+		nil,                 // arguments
+	)
+	utils.FailOnError(err, "Failed to declare a result queue")
+
+	ctx, cancel := context.WithCancel(context.Background())
+
 	return &Manager{
-		QueueMQ:   &q,
-		Scheduler: sched,
-		mq:        rabbitMQ,
-		ch:        ch,
+		TaskQueue:   &taskQueue,
+		ResultQueue: &resultQueue,
+		Scheduler:   sched,
+		mq:          rabbitMQ,
+		ch:          ch,
+		ctx:         ctx,
+		cancel:      cancel,
 	}
 }
 
 func (tm *Manager) StartTask(taskID uint) error {
 	// Send task ID to queue for processing
 	taskMessage := fmt.Sprintf(`{"task_id": %d}`, taskID)
-	err := tm.mq.SendMessage(tm.ch, tm.QueueMQ.Name, taskMessage)
+	err := tm.mq.SendMessage(tm.ch, tm.TaskQueue.Name, taskMessage)
 	utils.FailOnError(err, "Error starting task.")
 	return err
 }
@@ -56,11 +82,71 @@ func (tm *Manager) StartTaskWithScheduler(taskID uint, cronExpr string) {
 	_, err := tm.Scheduler.NewJob(cronExpr, taskID)
 	utils.FailOnError(err, "Error creating cron task")
 	taskMessage := fmt.Sprintf(`{"task_id": %d}`, taskID)
-	err = tm.mq.SendMessage(tm.ch, tm.QueueMQ.Name, taskMessage)
+	err = tm.mq.SendMessage(tm.ch, tm.TaskQueue.Name, taskMessage)
 	utils.FailOnError(err, "Error starting task.")
 }
 
+// GetResult получает сообщения из очереди результатов и выводит в консоль
+func (tm *Manager) GetResult() error {
+	msgs, err := tm.ch.Consume(
+		tm.ResultQueue.Name, // queue
+		"",                  // consumer
+		false,               // auto-ack - выключен для надёжности
+		false,               // exclusive
+		false,               // no-local
+		false,               // no-wait
+		nil,                 // args
+	)
+	if err != nil {
+		return fmt.Errorf("failed to register a consumer: %w", err)
+	}
+
+	log.Printf("Starting result consumer for queue: %s", tm.ResultQueue.Name)
+
+	go tm.processMessages(msgs)
+	return nil
+}
+
+// processMessages обрабатывает входящие сообщения
+func (tm *Manager) processMessages(msgs <-chan amqp091.Delivery) {
+	for {
+		select {
+		case <-tm.ctx.Done():
+			log.Println("Stopping result consumer...")
+			return
+		case msg, ok := <-msgs:
+			if !ok {
+				log.Println("Message channel closed")
+				return
+			}
+
+			// Обработка сообщения
+			log.Printf("Received result: %s", string(msg.Body))
+
+			// Acknowledge сообщение после обработки
+			if err := msg.Ack(false); err != nil {
+				log.Printf("Failed to acknowledge message: %v", err)
+			}
+		}
+	}
+}
+
+func (tm *Manager) CancelTask(taskID uint) error {
+	// Send task ID to queue for processing
+	taskMessage := fmt.Sprintf(`{"task_id": %d, "action": "cancel"}`, taskID)
+	err := tm.mq.SendMessage(tm.ch, tm.TaskQueue.Name, taskMessage)
+	utils.FailOnError(err, "Error canceling task.")
+	return err
+}
+
 func (tm *Manager) Shutdown() {
+	log.Println("Shutting down Task Manager...")
+
+	// Cancel context to stop consumers
+	if tm.cancel != nil {
+		tm.cancel()
+	}
+
 	// Close channel first
 	if tm.ch != nil {
 		if err := tm.ch.Close(); err != nil {
@@ -77,4 +163,6 @@ func (tm *Manager) Shutdown() {
 	if tm.Scheduler != nil {
 		tm.Scheduler.Shutdown()
 	}
+
+	log.Println("Task Manager shut down successfully")
 }
